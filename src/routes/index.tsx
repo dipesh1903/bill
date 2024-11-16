fd2e81c0dd8601280b7bed7452564ca4545bfc2f
Dipesh Agarwal <dipesh1903@gmail.com>
2024-11-16 18:43:17 +0530

    commented unused routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,4 @@
 import { createBrowserRouter, Navigate } from "react-router-dom";
-import Settings from "../pages/settings";
-import CompanySettings from "../pages/settings/company-settings";
-import ProductSettings from "../pages/settings/product-settings";
 import Stepper from "../components/stepper";
 import HomePage from "../pages/Home";
 
@@ -18,24 +15,25 @@ export const router = createBrowserRouter([
                 path: 'bill',
                 element: <Stepper />,
             },
-            {   
-                path: 'settings',
-                element: <Settings />,
-                children: [
-                    {
-                        index: true,
-                        element: <Navigate replace to="company" />
-                    },
-                    {
-                        path: 'company',
-                        element: <CompanySettings />
-                    },
-                    {
-                        path: 'product',
-                        element: <ProductSettings />
-                    }
-                ]
-            }
+            // For Later use
+            // {   
+            //     path: 'settings',
+            //     element: <Settings />,
+            //     children: [
+            //         {
+            //             index: true,
+            //             element: <Navigate replace to="company" />
+            //         },
+            //         {
+            //             path: 'company',
+            //             element: <CompanySettings />
+            //         },
+            //         {
+            //             path: 'product',
+            //             element: <ProductSettings />
+            //         }
+            //     ]
+            // }
         ]
     }]
 
