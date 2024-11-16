9219e6895ed63159f77cde44c039686bb06a2c94
Dipesh Agarwal <dipesh1903@gmail.com>
2024-11-16 16:33:30 +0530

    updated styles for save to local switch

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -39,7 +39,7 @@ export default function HomePage() {
                             }
                             />
                             <div className="relative w-11 h-6 bg-outline-high peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-outline-medium dark:peer-focus:ring-outline-medium rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-solid-high peer-checked:text-white"></div>
-                            <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Save Locally</span>
+                            <span className="ms-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Save Locally</span>
                         </InputLabel>
                     </div>
                     <Dialog>
